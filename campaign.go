ed6200fe6e5722a2d26b03546dcad9331d517d5a
raver119@gmail.com <raver119@gmail.com>
2020-09-14 16:38:15 +0300

    .gitignore

--- campaign.go
+++ campaign.go
@@ -1,9 +1,16 @@
 package classes
 
+import "encoding/json"
+
 type Campaign struct {
 	ID          int64  `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"desc"`
 	UserID      int64  `json:"user_id"`
 	Status      int8   `json:"status"`
+}
+
+func (c Campaign) ToJson() []byte {
+	js, _ := json.Marshal(c)
+	return js
 }
