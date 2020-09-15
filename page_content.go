a578c94b6205a4fa6c273726f8141a624bf4a187
raver119@gmail.com <raver119@gmail.com>
2020-09-15 08:46:45 +0300

    WIP: rollback EditableComponent in PageContent

--- page_content.go
+++ page_content.go
@@ -6,9 +6,11 @@ type PageContent struct {
 	Language  string `json:"language"`
 	SessionID string `json:"session_id"`
 
-	Title      string              `json:"title"`
-	Logo       string              `json:"logo"`
-	Components []EditableComponent `json:"components"`
+	Title string `json:"title"`
+	Logo  string `json:"logo"`
+
+	MainHighlight   string `json:"main_highlight"`
+	MainDescription string `json:"main_description"`
 }
 
 func (p PageContent) ToJson() []byte {
