7b88d5b1a9b953760cd0c6a4330bce974d365099
raver119@gmail.com <raver119@gmail.com>
2020-09-15 08:02:47 +0300

    WIP:

--- page_content.go
+++ page_content.go
@@ -6,13 +6,16 @@ type PageContent struct {
 	Language  string `json:"language"`
 	SessionID string `json:"session_id"`
 
-	Title           string `json:"title"`
-	Logo            string `json:"logo"`
-	MainSlogan      string `json:"main_slogan"`
-	MainDescription string `json:"main_description"`
+	Title      string              `json:"title"`
+	Logo       string              `json:"logo"`
+	Components []EditableComponent `json:"components"`
 }
 
 func (p PageContent) ToJson() []byte {
 	js, _ := json.Marshal(p)
 	return js
+}
+
+func (p PageContent) ToJsonString() string {
+	return string(p.ToJson())
 }
